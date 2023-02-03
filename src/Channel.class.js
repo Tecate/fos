4220c55d13245b2622af6125cc62836f31302be6
kawnsull <kawnsull@gmail.com>
2023-02-02 19:38:08 -0500

    formatting & revert windowExists changes

--- src/Channel.class.js
+++ src/Channel.class.js
@@ -1,154 +1,197 @@
-import { buildImage, buildChannel, buildText, buildLink, buildMedia, buildAttachment } from "../js/helpers.js";
+import {
+  buildImage,
+  buildChannel,
+  buildText,
+  buildLink,
+  buildMedia,
+  buildAttachment,
+} from "../js/helpers.js";
 
 class Channel extends HTMLElement {
-    constructor() {
-      super();
-      this.shadow = this.attachShadow({ mode: "open" });
-      this._data = [];
-      this.firstRun = true;
-      this.blockCount = 20;
-    }
-  
-    loadChannel(url, element) {
-        fetch(url)
-        .then((response) => {
-            const contentType = response.headers.get("content-type");
-            if (!contentType || !contentType.includes("application/json")) {
-            throw new TypeError("Oops, we haven't got JSON!");
-            }
-            if (response.ok == false)
-            document.getElementById("error").innerText =
-                "private block: " + response.status;
-            return response.json();
-        })
-        .then((data) => {
-            this._data = data;
-            var blockCount = this.blockCount;
-            for (var i = 0; i < data.contents.length; i++) {
-                var row = document.createElement("div");
-                row.classList.add("channel-row")
-                row._data = data.contents[i];
-                var windowExists = true;
-                if (document.querySelector(`fos-window[name="arena-${this._data.id}"]`) == undefined)
-                  windowExists = false;
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+    this._data = [];
+    this.firstRun = true;
+    this.blockCount = 20;
+  }
 
-                if (data.contents[i].class == "Image"){
-                    row.innerHTML += '<span><img src="img/channel-row-image.png" alt="Image"></span>'
-                    row.ondblclick = function() {
-                      if (windowExists) {
-                        var fosWindow = buildImage(this._data, false);
-                        fosWindow.open();
-                      } else {
-                        document.querySelector(`fos-window[name="arena-${this._data.id}"]`).open();
-                      }
-                    }
-                }
-                else if (data.contents[i].class == "Channel"){
-                    row.innerHTML += '<span><img src="img/arena-small.png" alt="Channel"></span>'
-                    row.ondblclick = function() {
-                      if (windowExists) {
-                        var fosWindow = buildChannel(this._data, false, blockCount);
-                        fosWindow.open();
-                      } else {
-                        document.querySelector(`fos-window[name="arena-${this._data.id}"]`).open();
-                      }
-                  }
-                } else if (data.contents[i].class == "Text"){
-                  row.innerHTML += '<span><img src="img/16x16/text.png" alt="Text"></span>'
-                  row.ondblclick = function() {
-                    if (windowExists) {
-                      var fosWindow = buildText(this._data, false);
-                      fosWindow.open();
-                    } else {
-                      document.querySelector(`fos-window[name="arena-${this._data.id}"]`).open();
-                    }
-                }
-              } else if (data.contents[i].class == "Link"){
-                row.innerHTML += '<span><img src="img/16x16/link.png" alt="Link"></span>'
-                row.ondblclick = function() {
-                  if (windowExists) {
-                    var fosWindow = buildLink(this._data, false);
-                    fosWindow.open();
-                  } else {
-                    document.querySelector(`fos-window[name="arena-${this._data.id}"]`).open();
-                  }
+  loadChannel(url, element) {
+    fetch(url)
+      .then((response) => {
+        const contentType = response.headers.get("content-type");
+        if (!contentType || !contentType.includes("application/json")) {
+          throw new TypeError("Oops, we haven't got JSON!");
+        }
+        if (response.ok == false)
+          document.getElementById("error").innerText =
+            "private block: " + response.status;
+        return response.json();
+      })
+      .then((data) => {
+        this._data = data;
+        var blockCount = this.blockCount;
+        for (var i = 0; i < data.contents.length; i++) {
+          var row = document.createElement("div");
+          row.classList.add("channel-row");
+          row._data = data.contents[i];
+
+          if (data.contents[i].class == "Image") {
+            row.innerHTML +=
+              '<span><img src="img/channel-row-image.png" alt="Image"></span>';
+            row.ondblclick = function () {
+              if (
+                document.querySelector(
+                  `fos-window[name="arena-${this._data.id}"]`
+                ) == undefined
+              ) {
+                var fosWindow = buildImage(this._data, false);
+                fosWindow.open();
+              } else {
+                document
+                  .querySelector(`fos-window[name="arena-${this._data.id}"]`)
+                  .open();
               }
-            } else if (data.contents[i].class == "Media"){
-              row.innerHTML += '<span><img src="img/16x16/media2.png" alt="Media"></span>'
-              row.ondblclick = function() {
-                if (windowExists) {
-                  var fosWindow = buildMedia(this._data, false);
-                  fosWindow.open();
-                } else {
-                  document.querySelector(`fos-window[name="arena-${this._data.id}"]`).open();
-                }
-            }
-          }else if (data.contents[i].class == "Attachment"){
-            row.innerHTML += '<span><img src="img/16x16/attachment.png" alt="Attachment"></span>'
-            row.ondblclick = function() {
-              if (windowExists) {
+            };
+          } else if (data.contents[i].class == "Channel") {
+            row.innerHTML +=
+              '<span><img src="img/arena-small.png" alt="Channel"></span>';
+            row.ondblclick = function () {
+              if (
+                document.querySelector(
+                  `fos-window[name="arena-${this._data.id}"]`
+                ) == undefined
+              ) {
+                var fosWindow = buildChannel(this._data, false, blockCount);
+                fosWindow.open();
+              } else {
+                document
+                  .querySelector(`fos-window[name="arena-${this._data.id}"]`)
+                  .open();
+              }
+            };
+          } else if (data.contents[i].class == "Text") {
+            row.innerHTML +=
+              '<span><img src="img/16x16/text.png" alt="Text"></span>';
+            row.ondblclick = function () {
+              if (
+                document.querySelector(
+                  `fos-window[name="arena-${this._data.id}"]`
+                ) == undefined
+              ) {
+                var fosWindow = buildText(this._data, false);
+                fosWindow.open();
+              } else {
+                document
+                  .querySelector(`fos-window[name="arena-${this._data.id}"]`)
+                  .open();
+              }
+            };
+          } else if (data.contents[i].class == "Link") {
+            row.innerHTML +=
+              '<span><img src="img/16x16/link.png" alt="Link"></span>';
+            row.ondblclick = function () {
+              if (
+                document.querySelector(
+                  `fos-window[name="arena-${this._data.id}"]`
+                ) == undefined
+              ) {
+                var fosWindow = buildLink(this._data, false);
+                fosWindow.open();
+              } else {
+                document
+                  .querySelector(`fos-window[name="arena-${this._data.id}"]`)
+                  .open();
+              }
+            };
+          } else if (data.contents[i].class == "Media") {
+            row.innerHTML +=
+              '<span><img src="img/16x16/media2.png" alt="Media"></span>';
+            row.ondblclick = function () {
+              if (
+                document.querySelector(
+                  `fos-window[name="arena-${this._data.id}"]`
+                ) == undefined
+              ) {
+                var fosWindow = buildMedia(this._data, false);
+                fosWindow.open();
+              } else {
+                document
+                  .querySelector(`fos-window[name="arena-${this._data.id}"]`)
+                  .open();
+              }
+            };
+          } else if (data.contents[i].class == "Attachment") {
+            row.innerHTML +=
+              '<span><img src="img/arena-small.png" alt="Attachment"></span>';
+            row.ondblclick = function () {
+              if (
+                document.querySelector(
+                  `fos-window[name="arena-${this._data.id}"]`
+                ) == undefined
+              ) {
                 var fosWindow = buildAttachment(this._data, false);
                 fosWindow.open();
               } else {
-                document.querySelector(`fos-window[name="arena-${this._data.id}"]`).open();
+                document
+                  .querySelector(`fos-window[name="arena-${this._data.id}"]`)
+                  .open();
               }
+            };
+          } else {
+            row.innerHTML += `<span class="class">${data.contents[i].class}</span>`;
           }
+          row.innerHTML += ` <span class="id">${data.contents[i].id}</span>`;
+          row.innerHTML += ` <span class="title">${data.contents[i].title}</span>`;
+          row.innerHTML += ` <button class="button-save"><img src="img/16x16/briefcase-1.png"></span>`;
+
+          element.appendChild(row);
         }
-                else {
-                    row.innerHTML += `<span class="class">${data.contents[i].class}</span>`;
-                }
-                row.innerHTML += ` <span class="id">${data.contents[i].id}</span>`;
-                row.innerHTML += ` <span class="title">${data.contents[i].title}</span>`;
-                row.innerHTML += ` <button class="button-save"><img src="img/16x16/briefcase-1.png"></span>`;
+        this.parentElement.bringInbounds();
+        console.log("API call:", this._data);
+      });
+  }
 
-                element.appendChild(row);
-            }
-            this.parentElement.bringInbounds();
-            console.log("API call:", this._data)
-        });
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.render();
+  }
 
-    }
-    
-    attributeChangedCallback(name, oldValue, newValue) {
-      this.render();
-    }
-  
-    connectedCallback() {
-      this.render();
-    }
+  connectedCallback() {
+    this.render();
+  }
 
-    // static get observedAttributes() {
-    //   return ["title", "id"];
-    // }
-  
-    // get title() {
-    //   return this.hasAttribute("title") ? this.getAttribute("title") : null;
-    // }
-  
-    // set title(val) {
-    //   if (val) this.setAttribute("title", val);
-    //   else this.removeAttribute("title");
-    // }
+  // static get observedAttributes() {
+  //   return ["title", "id"];
+  // }
 
-    // get id() {
-    //     return this.hasAttribute("id") ? this.getAttribute("id") : null;
-    // }
-    
-    // set id(val) {
-    //     if (val) this.setAttribute("id", val);
-    //     else this.removeAttribute("id");
-    // }
+  // get title() {
+  //   return this.hasAttribute("title") ? this.getAttribute("title") : null;
+  // }
 
-    set data(value) {
-        this._data = value;
-      }
-      
-    get data() {
-        return this._data;
-    }
+  // set title(val) {
+  //   if (val) this.setAttribute("title", val);
+  //   else this.removeAttribute("title");
+  // }
 
-    render() {
-      this.shadow.innerHTML = `
+  // get id() {
+  //     return this.hasAttribute("id") ? this.getAttribute("id") : null;
+  // }
+
+  // set id(val) {
+  //     if (val) this.setAttribute("id", val);
+  //     else this.removeAttribute("id");
+  // }
+
+  set data(value) {
+    this._data = value;
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  render() {
+    this.shadow.innerHTML = `
           <style>
               :host{
                   display: block;
@@ -243,65 +286,66 @@ class Channel extends HTMLElement {
           <slot></slot>
       `;
 
-      var channelElement = this;
-      var data = this._data;
-      var page = 1;
-      var channelURL = "https://api.are.na/v2/channels/" + this._data.id;
-      var pageCount = Math.ceil(this._data.length / this.blockCount);
-      var header = document.createElement("div");
-      var viewSelector = document.createElement("select");
-      var buttonArenaLink = document.createElement("div");
-      var loadButton = document.createElement("div");
-      var contents = document.createElement("div");
-      var footer = document.createElement("div");
+    var channelElement = this;
+    var data = this._data;
+    var page = 1;
+    var channelURL = "https://api.are.na/v2/channels/" + this._data.id;
+    var pageCount = Math.ceil(this._data.length / this.blockCount);
+    var header = document.createElement("div");
+    var viewSelector = document.createElement("select");
+    var buttonArenaLink = document.createElement("div");
+    var loadButton = document.createElement("div");
+    var contents = document.createElement("div");
+    var footer = document.createElement("div");
 
-      // header.innerHTML = `${this._data.title} <a href="https://are.na/${this._data.owner_slug}/${this._data.slug}"><img src="img/arena-small.png"></a>`
-      header.id = "channel-header";
-      this.shadow.appendChild(header);
-      
-      viewSelector.innerHTML = 
-      `<option value="both">Both</option>
+    // header.innerHTML = `${this._data.title} <a href="https://are.na/${this._data.owner_slug}/${this._data.slug}"><img src="img/arena-small.png"></a>`
+    header.id = "channel-header";
+    this.shadow.appendChild(header);
+
+    viewSelector.innerHTML = `<option value="both">Both</option>
       <option value="channels">Channels</option>
       <option value="blocks">Blocks</option>`;
-      header.appendChild(viewSelector);
+    header.appendChild(viewSelector);
 
-      buttonArenaLink.classList.add("button");
-      buttonArenaLink.innerHTML = '<img src="img/arena-small.png">';
-      buttonArenaLink.onclick = function() {
-        window.open("https://are.na/" + data.owner_slug + "/" + data.slug, '_blank')
-      }
-      header.appendChild(buttonArenaLink);
+    buttonArenaLink.classList.add("button");
+    buttonArenaLink.innerHTML = '<img src="img/arena-small.png">';
+    buttonArenaLink.onclick = function () {
+      window.open(
+        "https://are.na/" + data.owner_slug + "/" + data.slug,
+        "_blank"
+      );
+    };
+    header.appendChild(buttonArenaLink);
 
-      contents.id = "channel-contents";
-      this.shadow.appendChild(contents);
+    contents.id = "channel-contents";
+    this.shadow.appendChild(contents);
 
-      if (this.firstRun) {
-        this.loadChannel(channelURL + "?page=" + page, contents);
-        page += 1;
-        this.firstRun = false;
-      }
-
-      loadButton.classList.add("button");
-      loadButton.innerText = `Load page ${page}/${pageCount}`;
-      header.appendChild(loadButton);
-    
-      loadButton.onclick = function() {
-        channelElement.loadChannel(channelURL + "?page=" + page, contents);
-        if (page < pageCount) {
-          page += 1;
-          loadButton.innerText = `Load page ${page}/${pageCount}`;
-        } else {
-          loadButton.innerText = "nothing more to load";
-          loadButton.onclick = function() {}
-        }
-      }
-
-      footer.id = "footer";
-      footer.innerHTML = `<span class="inset">${data.length} object(s)</span>
-      <span class="inset">owner: ${data.owner_slug}</span>`
-      this.shadow.appendChild(footer);
+    if (this.firstRun) {
+      this.loadChannel(channelURL + "?page=" + page, contents);
+      page += 1;
+      this.firstRun = false;
     }
+
+    loadButton.classList.add("button");
+    loadButton.innerText = `Load page ${page}/${pageCount}`;
+    header.appendChild(loadButton);
+
+    loadButton.onclick = function () {
+      channelElement.loadChannel(channelURL + "?page=" + page, contents);
+      if (page < pageCount) {
+        page += 1;
+        loadButton.innerText = `Load page ${page}/${pageCount}`;
+      } else {
+        loadButton.innerText = "nothing more to load";
+        loadButton.onclick = function () {};
+      }
+    };
+
+    footer.id = "footer";
+    footer.innerHTML = `<span class="inset">${data.length} object(s)</span>
+      <span class="inset">owner: ${data.owner_slug}</span>`;
+    this.shadow.appendChild(footer);
   }
-  
-  customElements.define("fos-channel", Channel);
-  
+}
+
+customElements.define("fos-channel", Channel);
